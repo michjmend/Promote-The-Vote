c928988c15ab1993bb797e79544d26620fb3d5c3
Peanut <michjmend@gmail.com>
2019-07-09 21:08:18 -0400

    component sidenav

---
+++ src/components/SideNavbar/SideNavabr.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SideNavbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Promote the Vote SideNavbar
+      </a>
+    </nav>
+  );
+}
+
+export default SideNavbar;
