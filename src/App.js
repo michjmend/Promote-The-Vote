fcf46e3a7734fdd35aeaa68a86805add7912e1d1
FULLSTACK-SY <sushmitayande@gmail.com>
2019-07-11 00:16:31 -0400

    basic setup of app.js and created some required js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ReactDOM from 'react-dom';
+
+import DisplayPosts from "./components/DisplayPosts";
+import CreatePost from "./components/CreatePost";
+import EditPost from "./components/EditPost";
+import DeletePost from "./components/DeletePost";
+
+
+// use normal bootstrap if required. give yarn add bootstrap for below...
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+ import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <>
+          <h2>Promote-The-Vote</h2>
+          <nav>
+              <img src={logo} width="30" height="30" alt="PromoteTheVote" />
+              <ul>
+                <li>
+                  <Link to='/'>Display Posts</Link>
+                </li>
+                <li>
+                  <Link to='/create'>Create New Post</Link>
+                </li>
+                <li>
+                  <Link to='/edit'>Edit Post</Link>
+                </li>
+                <li>
+                  <Link to='/delete'>Delete Post</Link>
+                </li>
+              </ul>
+          </nav>
+          <Route path="/" exact component={DisplayPosts} />
+          <Route path="/create" component={CreatePost} />
+          <Route path="/edit/:id" component={EditPost} />
+          <Route path="/delete/:id" component={DeletePost} />
+        </>
+      </Router>
   );
 }
 
---
+++ src/components/DisplayPosts.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class DisplayPosts extends Component {
+    render() {
+        return (
+          <div>
+              <p>Welcome to Display Posts component!</p>
+          </div>
+
+        )
+    }
+}
