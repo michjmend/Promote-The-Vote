02dc71c8baf65906da9b588572eba78582e28c83
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-07-16 21:51:55 -0400

    added a router for heroku launch

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express")
 
 const app = express()
-
+const router = require("express").Router();
 // Connect to our database
 
 // Init middleware
 app.use(express.json({ extended: false }))
 
 // Routes
+
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "/client/public/index.html"));
+});
+
+app.use(router)
 
 const PORT = process.env.PORT || 5000
 
