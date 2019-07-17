58025fca20d886905453cec4565d6ac3f88ed212
FULLSTACK-SY <sushmitayande@gmail.com>
2019-07-17 07:44:42 -0400

    revised models using MongoDB i/o MySQL

--- models/post.js
+++ models/post.js
@@ -1,30 +1,32 @@
-module.exports = (sequelize, DataTypes) => {
-  var Post = sequelize.define("Post", {
+const mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+const Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+const PostSchema = new Schema({
     title: {
-      type: DataTypes.STRING,
-      allowNull: false
+      type: String,
+      required: true
     },
     review: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        min: 0
-      }
+      type: String,
+      required: true
     },
     picture: {
-      type: DataTypes.STRING
+      type: String,
+      required: false
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
     }
   });
 
-  Post.associate = models => {
-    Post.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
 
 
-  };
+  // This creates our model from the above schema, using mongoose's model method
+var Post = mongoose.model("Post", PostSchema);
 
-  return Post;
-};
+// Export the Note model
+module.exports = Post;
