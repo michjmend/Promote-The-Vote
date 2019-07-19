ae9c13da2099a9eec9c3ad7a9e226bf3b0832ca1
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-07-18 21:35:51 -0400

    scraper logic added to contextfolder

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
+
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+require('dotenv').config()
+
+// var router = express.Router();
+
 const express = require("express")
 
 const app = express()
-const router = require("express").Router();
-// Connect to our database
+
+
+// Require all models
+var db = require("./client/src/components/Scrapper");
+
+
+app.use("/api/scraper", require("./routes/scraper"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI);
+
+
 
 // Init middleware
 app.use(express.json({ extended: false }))
 
-// Routes
-
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "/client/public/index.html"));
-});
-
-app.use(router)
 
 const PORT = process.env.PORT || 5000
 
