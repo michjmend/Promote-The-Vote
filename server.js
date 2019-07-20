391c5ed82e7062d165f1374e0c1fd3faf1abf5c6
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-07-20 12:06:03 -0400

    need andrews server stuff

--- server.js
+++ server.js
@@ -1,50 +1,37 @@
 
-var mongoose = require("mongoose");
-
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-
-// var router = express.Router();
-
 const express = require("express")
 const connectDB = require("./config/db")
 const app = express()
 
 
-// Require all models
-var db = require("./client/src/components/Scraper");
-
-
-app.use("/api/scraper", require("./routes/scraper"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
+// Connect to our database
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI;
+var MONGODB_URI = process.env.MONGODB_URI  || connectDB();
+
+// "mongodb://promotethevote:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7";
 
 mongoose.connect(MONGODB_URI);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+}
 
-
-// Connect to our database
-connectDB()
 
 // Init middleware
 app.use(express.json({ extended: false }))
 app.get("/", (req,res) =>
-  res.json({ msg: "Welcome to the Promote-the-Vote API" })
+ res.json({ msg: "Welcome to the Promote-the-Vote API" })
 )
 // Routes
 app.use("/api/users", require("./routes/users"))
 // router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "/client/public/index.html"));
+//   res.sendFile(path.join(__dirname, “/client/public/index.html”));
 // });
 
 // app.use(router)
+
+
 
 
 const PORT = process.env.PORT || 5000
